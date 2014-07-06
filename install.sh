3a0fcf8d2c4a13b3893a71911c0ae2c8ef5e329e
Nick Macbook Pro <njachowski@gmail.com>
2014-07-06 17:43:48 +0800

    added .tmux.conf file

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ cp ~/vm_config/.bash_profile ~/.bash_profile
 cp ~/vm_config/.bashrc ~/.bashrc
 cp ~/vm_config/.Rprofile ~/.Rprofile
 cp ~/vm_config/.vimrc ~/.vimrc
+cp ~/vm_config/.tmux.conf ~/.tmux.conf
 source ~/.bash_profile
 source ~/.bashrc
 echo "installed .bash_profile, .bashrc, .Rprofile, .vimrc"
+tmux source-file ~/.tmux.conf
 
 # clean-up 
 echo "cleaning up and removing vm_config folder"
