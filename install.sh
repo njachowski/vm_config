97047d805d885aeab023a320f38c239303729b04
Nick Macbook Pro <njachowski@gmail.com>
2014-07-06 12:34:15 +0800

    small change

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 cp ~/vm_config/.bash_profile ~/.bash_profile
 cp ~/vm_config/.bashrc ~/.bashrc
-source ~/.bashrc
 cp ~/vm_config/.Rprofile ~/.Rprofile
+source ~/.bash_profile
+source ~/.bashrc
+echo "installed .bash_profile, .bashrc, .Rprofile"
 
 # clean-up 
+echo "cleaning up and removing vm_config folder"
 rm -r ~/vm_config
