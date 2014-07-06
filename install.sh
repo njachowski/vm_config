9298d580cef892cdf1837da9d26c1bfe87923843
Nick Macbook Pro <njachowski@gmail.com>
2014-07-06 13:37:21 +0800

    changed install

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 cp ~/vm_config/.bash_profile ~/.bash_profile
 cp ~/vm_config/.bashrc ~/.bashrc
 cp ~/vm_config/.Rprofile ~/.Rprofile
+cp ~/vm_config/.vimrc ~/.vimrc
 source ~/.bash_profile
 source ~/.bashrc
-echo "installed .bash_profile, .bashrc, .Rprofile"
+echo "installed .bash_profile, .bashrc, .Rprofile, .vimrc"
 
 # clean-up 
 echo "cleaning up and removing vm_config folder"
